23693598da7ca1d0683b4c15541516830da6c34c
Abdallah Ali <amaali1991@gmail.com>
2023-06-19 10:15:37 +0300

    22.05 -> 23.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
     flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus";
 
     flake-compat = {
@@ -11,32 +11,35 @@
     };
   };
 
-  outputs = inputs:
-    let
-      core-inputs = inputs // {
+  outputs = inputs: let
+    core-inputs =
+      inputs
+      // {
         src = ./.;
       };
 
-      # Create the library, extending the nixpkgs library and merging
-      # libraries from other inputs to make them available like
-      # `lib.flake-utils-plus.mkApp`.
-      # Usage: mkLib { inherit inputs; src = ./.; }
-      #   result: lib
-      mkLib = import ./lib core-inputs;
+    # Create the library, extending the nixpkgs library and merging
+    # libraries from other inputs to make them available like
+    # `lib.flake-utils-plus.mkApp`.
+    # Usage: mkLib { inherit inputs; src = ./.; }
+    #   result: lib
+    mkLib = import ./lib core-inputs;
 
-      # A convenience wrapper to create the library and then call `lib.mkFlake`.
-      # Usage: mkFlake { inherit inputs; src = ./.; ... }
-      #   result: <flake-outputs>
-      mkFlake = flake-and-lib-options@{ inputs, src, ... }:
-        let
-          lib = mkLib {
-            inherit inputs src;
-          };
-          flake-options = builtins.removeAttrs flake-and-lib-options [ "inputs" "src" ];
-        in
-          lib.mkFlake flake-options;
+    # A convenience wrapper to create the library and then call `lib.mkFlake`.
+    # Usage: mkFlake { inherit inputs; src = ./.; ... }
+    #   result: <flake-outputs>
+    mkFlake = flake-and-lib-options @ {
+      inputs,
+      src,
+      ...
+    }: let
+      lib = mkLib {
+        inherit inputs src;
+      };
+      flake-options = builtins.removeAttrs flake-and-lib-options ["inputs" "src"];
     in
-    {
-      inherit mkLib mkFlake;
-    };
+      lib.mkFlake flake-options;
+  in {
+    inherit mkLib mkFlake;
+  };
 }
